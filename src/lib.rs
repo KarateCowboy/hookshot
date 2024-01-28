36782ce3758d9a120ff277a179fe2f73b62af7f8
Matthew Origer <moriger@mykolab.com>
2024-01-28 16:19:45 -0600

    WIP squash later

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use url::{Host, Url};
 
-
 pub struct Video {
-    pub site_id: String,
+    pub platform: Platform,
+    // pub asset_id: String
 }
 
 #[derive(PartialEq, Eq, Debug)]
@@ -21,9 +21,11 @@ pub fn grok_url_arg(url_str: String) -> Url {
 
 pub fn video_from_url(video_url: &str) -> Video {
     let my_url = grok_url_arg(video_url.to_owned().clone());
-    // my_url.
+    let host_val = my_url.host_str().clone();
+    println!("{:?}", host_val);
     return Video {
-        site_id: String::from("blarg"),
+        platform: parse_platform(host_val.map(|s| s.to_string())),
+        // asset_id: parse_asset_id()
     };
 }
 pub fn parse_platform(host_str: Option<String>) -> Platform {
@@ -31,13 +33,20 @@ pub fn parse_platform(host_str: Option<String>) -> Platform {
         Some(val) => {
             let val_lower = val.to_lowercase();
             match val_lower.as_str() {
-                "youtube.com" => Platform::YouTube,
-                "rumble.com" => Platform::Rumble,
-                "nicovideo.jp" => Platform::NicoVideo,
-                "bitchute.com" => Platform::BitChute,
+                "www.youtube.com" => Platform::YouTube,
+                "www.rumble.com" => Platform::Rumble,
+                "www.nicovideo.jp" => Platform::NicoVideo,
+                "www.bitchute.com" => Platform::BitChute,
                 _ => Platform::Unknown,
             }
         }
         None => Platform::Unknown,
     }
+}
+
+pub fn parse_asset_id(url: &Url) -> Option<String> {
+    return match url.query() {
+        Some(v) => Some(String::from("found")),
+        None => None,
+    };
 }
--- tests/get_video_info_test.rs
+++ tests/get_video_info_test.rs
@@ -2,7 +2,7 @@ use spectral::prelude::*;
 
 use url::Url;
 
-use hookshot::{grok_url_arg, video_from_url, parse_platform, Video, Platform};
+use hookshot::{grok_url_arg, parse_asset_id, parse_platform, video_from_url, Platform, Video};
 
 #[test]
 fn grok_url_string() {
@@ -10,40 +10,50 @@ fn grok_url_string() {
     let result = grok_url_arg(video_url.clone());
     assert_that!(video_url.parse::<Url>()).is_ok();
 }
-#[test]
-fn builds_a_video_object_from_url_string() {
-    let video_url = String::from("https://www.youtube.com/watch?v=Fi5eYLA0uAg");
-    let result_video : Video = video_from_url(&video_url);
-    assert_that!(true).is_true();
-    // assert_that!(&result_video.platform).is_equal_to(Platforms::YouTube);
-}
 
 #[test]
-fn parse_youtube_platform(){
-    let parsed_platform : Platform = parse_platform(Some("youtube.com".to_string()));
+fn parse_youtube_platform() {
+    let parsed_platform: Platform = parse_platform(Some("www.youtube.com".to_string()));
     assert_that!(parsed_platform).is_equal_to(Platform::YouTube);
 }
 #[test]
-fn parse_rumble_platform(){
-    let parsed_platform : Platform = parse_platform(Some("rumble.com".to_string()));
+fn parse_rumble_platform() {
+    let parsed_platform: Platform = parse_platform(Some("www.rumble.com".to_string()));
     assert_that!(parsed_platform).is_equal_to(Platform::Rumble);
-
 }
 #[test]
-fn parse_nico_platform(){
-    let parsed_platform : Platform = parse_platform(Some("nicovideo.jp".to_string()));
+fn parse_nico_platform() {
+    let parsed_platform: Platform = parse_platform(Some("www.nicovideo.jp".to_string()));
     assert_that!(parsed_platform).is_equal_to(Platform::NicoVideo);
 }
 #[test]
-fn parse_bitchute_platform(){
-    let parsed_platform : Platform = parse_platform(Some("bitchute.com".to_string()));
+fn parse_bitchute_platform() {
+    let parsed_platform: Platform = parse_platform(Some("www.bitchute.com".to_string()));
     assert_that!(parsed_platform).is_equal_to(Platform::BitChute);
 }
 #[test]
-fn parse_unkown_platform(){
-    let parsed_platform : Platform = parse_platform(Some("geocities.com".to_string()));
+fn parse_unkown_platform() {
+    let parsed_platform: Platform = parse_platform(Some("www.geocities.com".to_string()));
     assert_that!(parsed_platform).is_equal_to(Platform::Unknown);
 }
 
+#[test]
+fn parse_query_yt_asset_id() {
+    let video_url = grok_url_arg(String::from("https://www.youtube.com/watch?v=Fi5eYLA0uAg"));
+    let asset_id = parse_asset_id(&video_url);
+    println!("{:?}", asset_id);
+}
+#[test]
+fn parse_segment_yt_asset_id() {
+    let video_url = grok_url_arg(String::from("https://youtu.be/Fi5eYLA0uAg?si=TMmFVQ6R7Zi8_6XV"));
+    let asset_id = parse_asset_id(&video_url);
+    println!("{:?}", asset_id);
+}
 
-
+#[test]
+fn builds_a_video_object_from_url_string() {
+    let video_url = String::from("https://www.youtube.com/watch?v=Fi5eYLA0uAg");
+    let result_video: Video = video_from_url(&video_url);
+    assert_that!(result_video.platform).is_equal_to(Platform::YouTube);
+    // assert_that!(result_video.asset_id).is_equal_to(String::from("Fi5eYLA0uAg"));
+}
